f44e5db7759ab1324ebb154e022d490682eb5a46
nicole <nicole.k.lawrence@outlook.com>
2022-07-26 19:45:20 +1000

    Added setters to complete that part of the interface.

--- FiringSolution.java
+++ FiringSolution.java
@@ -9,14 +9,14 @@ import java.text.DecimalFormat;
 
 public class FiringSolution implements FiringSolutionInterface
 {
-    private static final DecimalFormat FMT=new DecimalFormat("###0.000");
+    private static final DecimalFormat FMT = new DecimalFormat("###0.000");
     
     private Time time;
     private int degree;
     private double x;
     private double y;
     
-    public Time getTIme() { 
+    public Time getTime() { 
         return time; 
     }   
     //prompts the user to enter the value of time in hours
@@ -46,5 +46,29 @@ public class FiringSolution implements FiringSolutionInterface
     //prompts the user to enter the value of y's point
     public void yInput(double input) { 
         y = input; 
+    }
+    
+    @Override
+    public void setTime(Time t) {
+        time = t;
+        
+    }
+    
+    @Override
+    public void setDegree(int d) {
+        degree = d;
+        
+    }
+    
+    @Override
+    public void setX(double x) {
+        this.x = x;
+        
+    }
+    
+    @Override
+    public void setY(double y) {
+       this.y = y;
+        
     }
  }
