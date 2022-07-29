1eb247a8b78bf75b2c9a6901e5f5ad4e98da1518
nicole <nicole.k.lawrence@outlook.com>
2022-07-29 15:22:20 +1000

    Added algorithm from notes as comments.

--- AssigOne222.java
+++ AssigOne222.java
@@ -6,10 +6,31 @@
  * Split <<50:50 or change!>>
  */
 
+import java.util.Scanner;
+
 public class AssigOne222
 {
     public static void main(String []args)
     {
         // COMPLETE ME!
+
+//         
+// set up the launch time
+// iterating through each position (degree) of the missile from Launchtown to Doomsville 
+
+// and for each position/degree repeat the following steps until a solution is found or it is 
+// determined there are no solutions
+// • calculating how the laser would need to move to get there
+// • calculating how the missile would need to move to get there
+// • determining how many seconds the movement of each item 
+// would take and, if the laser can get there no later than the 
+// missile
+// o determine the (x,y) coordinate of impact and the time
+// o create a firing solution of these values
+// o display the time and firing solution (you can make use 
+// of the defined DecimalFormat objects to format the 
+// output of Time and FiringSolution objects)
+// • otherwise 
+// o moving the missile to the next location (degree)
     }
 }
