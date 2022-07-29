28e0e00f521d624e3cc1f8d82ceca8646ce7e699
nicole <nicole.k.lawrence@outlook.com>
2022-07-29 15:00:56 +1000

    Added possible toString method

--- FiringSolution.java
+++ FiringSolution.java
@@ -16,46 +16,39 @@ public class FiringSolution extends Time implements FiringSolutionInterface
     private double x;
     private double y;
     
+    // gets current time
     public Time getTime() { 
         return time; 
     }   
-    //prompts the user to enter the value of time in hours
-    public void timeInput(Time input) { 
-        time = input;
-    }
+        
+    public void setTime(Time t) {
+        time = t;
+        
+    }  
 
-    public int getDegree() { 
-        return degree; 
-    }
     //prompts the user to enter the value of degree
     public void degreeInput(int input) { 
         degree = input; 
     }
 
-    public double getX() { 
-        return x;
+    public int getDegree() { 
+        return degree; 
     }
+
+    
+    public void setDegree(int d) {
+        degree = d;
+        
+    }
+
+    
     //prompts the user to enter the value of x's point
     public void xInput(double input) { 
         x = input;
     }
 
-    public double getY() { 
-        return y;
-    }
-    //prompts the user to enter the value of y's point
-    public void yInput(double input) { 
-        y = input; 
-    }    
-    
-    public void setTime(Time t) {
-        time = t;
-        
-    }    
-    
-    public void setDegree(int d) {
-        degree = d;
-        
+    public double getX() { 
+        return x;
     }
     
     public void setX(double x) {
@@ -63,8 +56,24 @@ public class FiringSolution extends Time implements FiringSolutionInterface
         
     }
     
+
+    //prompts the user to enter the value of y's point
+    public void yInput(double input) { 
+        y = input; 
+    }    
+  
+    public double getY() { 
+        return y;
+    }
+    
     public void setY(double y) {
        this.y = y;
         
+    }    
+
+    public String toString()
+    {
+        return this.getTime().toString();
     }
+
  }
